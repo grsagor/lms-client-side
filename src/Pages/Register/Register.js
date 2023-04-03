e7af4d7dff8c73967224f9cd976e7e956e247e54
nusratnova5 <nusrat.nova16@gmail.com>
2023-04-03 23:10:55 +0600

    email-password and google authentication implemented

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import img1 from '../Register/Images/img1.png';
 import img2 from '../Register/Images/img2.png';
 import UseTittle from '../../Hooks/UseTittle';
+import { AuthContext } from '../../Context/AuthProvider';
 
 
 const Register = () => {
     UseTittle('Register');
     const { register, handleSubmit, formState: { errors } } = useForm();
+    const {createUser} = useContext(AuthContext);
+    
+    const handleSignUp = (data)=>{
+        console.log(data);
+           createUser(data.email,data.password)
+            .then(result =>{
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(error =>console.log(error));
+        }
 
     return (
         <div>
@@ -20,7 +32,7 @@ const Register = () => {
 
                     <div className="card flex-shrink-0 w-full max-w-md shadow-2xl ml-3 bg-base-100">
 
-                        <form className='card-body' onSubmit={handleSubmit()}>
+                        <form className='card-body' onSubmit={handleSubmit(handleSignUp)}>
                             <div className='self-center '>
                                 <img className='h-16 w-16 ' src={img2} alt="" />
                             </div>
@@ -30,10 +42,10 @@ const Register = () => {
                                 <input {...register("name")} type="text" placeholder="Enter name" className="input w-full" />
                             </div>
 
-                            <div className="form-control w-full max-w-xs py-3">
-                                {/* <label className="label"><span className="label-text">Enter Picture</span></label> */}
+                            {/* <div className="form-control w-full max-w-xs py-3">
+                                <label className="label"><span className="label-text">Enter Picture</span></label>
                                 <input {...register("image")} type="file" placeholder="Enter Picture" className="input w-full" />
-                            </div>
+                            </div> */}
 
                             <div className="form-control w-full">
                                 {/* <label className="label"><span className="label-text">Email</span></label> */}
@@ -44,6 +56,17 @@ const Register = () => {
                                 {/* <label className="label"><span className="label-text">Password</span></label> */}
                                 <input {...register("password")} type="password" placeholder="Enter password" className="input w-full" />
                             </div>
+
+                            <div className="form-control w-full max-w-xs mt-5">
+                        <label className="label">
+                            <span className="label-text">Are you a teacher or a student?</span>
+                        </label>
+                        <select {...register("role")} className="select select-bordered">
+                            <option disabled selected>Select one</option>
+                            <option>Teacher</option>
+                            <option>Student</option>
+                        </select>
+                    </div>
 
                             <input className='btn border-none w-full mt-3 orange-bg  text-white' value='REGISTER' type="submit" />
                         </form>
